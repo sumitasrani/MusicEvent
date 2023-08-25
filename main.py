d0a03fda326ab44da43217f9e1f7e23a4c98cea0
sumitasrani <hello@sumitasrani.com>
2023-08-26 00:21:00 +0530

    Store events on new event in a text file, send email on new event

--- main.py
+++ main.py
@@ -7,19 +7,37 @@ URL = "http://programmer100.pythonanywhere.com/tours/"
 def scrape(url):
     """" Scrape the page source from the URL """
     response = requests.get(url)
-    text = response.text
-    return text
+    source = response.text
+    return source
 
-def extract(text):
+
+def extract(source):
     """ Extract data from the scrapped source file """
     extractor = selectorlib.Extractor.from_yaml_file("extract.yaml")
-    value = extractor.extract(text)["tours"]
+    value = extractor.extract(source)["tours"]
     return value
+
+
+def email_sent():
+    print("email")
+
+
+def store(event):
+    with open("data.txt", 'a') as file:
+        file.write(event + "\n")
+
+
+def read(event):
+    with open("data.txt", 'r') as file:
+        return file.read(event)
+
 
 if __name__ == "__main__":
     scraped = scrape(URL)
     event_check = extract(scraped)
     print(event_check)
-
-
-
+    content = read(event_check)
+    if event_check != "No upcoming tours":
+        if event_check not in content:
+            store(event_check)
+            email_sent()
